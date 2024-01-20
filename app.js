66fbe08f431d64125a555bebf1922274469f8b72
David <david.williamx@gmail.com>
2024-01-20 20:30:23 -0300

    Adiciona resposta para o desafio 3.

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 // Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!".
 // Caso contrário, mostre "Boa semana!".
 /*
-console.log('>>> Início do programa.');
+    console.log('>>> Início do programa.');
 
-let dia = prompt('Informe qual o dia semana: ');
-console.log('>>> Informado qual o dia da semana pelo usuário.');
+    let dia = prompt('Informe qual o dia semana: ');
+    console.log('>>> Informado qual o dia da semana pelo usuário.');
 
-if (dia === 'sabado' || dia === 'Sabado' || dia === 'sábado' || dia === 'Sábado' || dia === 'Domingo' || dia === 'domingo' ) {
-    alert('Bom fim de semana!');
-} else {
-    alert('Boa semana!');
-}
-console.log('>>> Fim do programa.');
+    if (dia === 'sabado' || dia === 'Sabado' || dia === 'sábado' || dia === 'Sábado' || dia === 'Domingo' || dia === 'domingo' ) {
+        alert('Bom fim de semana!');
+    } else {
+        alert('Boa semana!');
+    }
+    console.log('>>> Fim do programa.');
 */
-//
+//------------------------------------------------------------------------
 
 /**
  *  Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
  *
-*/
+
 
 console.log('>>> Inicio do programa.')
 console.log('>>> ')
@@ -41,12 +41,44 @@ if (numero < 0) {
 
 console.log('>>> Fim do programa.')
 console.log('>>> ');
-//
 
+ */
+//------------------------------------------------------------------------
 
-// Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!".
-// Caso contrário, mostre "Tente novamente para ganhar.".
+/**
+// Crie um sistema de pontuação para um jogo.
+*  Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!".
+*  Caso contrário, mostre "Tente novamente para ganhar.".
+*/
+
+console.log('>>> Inicio do programa.');
+console.log('>>> ');
+console.log('>>> Aguardando entrada do usuário.');
+console.log('>>> ');
+let pontos = Number(prompt('Informe a sua pontuação: '));
+
+console.log('>>> Processando pontuação.');
+console.log('>>> ');
+if (pontos >= 100) {
+    alert(`Parabéns, você venceu!`);
+    console.log('>>> Usuário vencedor. ')
+} else {
+    alert(`Tente novamente para ganhar!`);
+    console.log('>>> Usuário não vencedor. ')
+}
+console.log('>>> Fim do programa.');
+console.log('>>> ');
+
+//------------------------------------------------------------------------
 
 // Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
 
+
+
+//------------------------------------------------------------------------
+
 // Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+
+
+
+//------------------------------------------------------------------------

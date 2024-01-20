8630db2ced71047d5d31ced8fdd12cb65479076d
David <david.williamx@gmail.com>
2024-01-20 20:07:58 -0300

    Adiciona resposta para o desafio 1.

---
+++ app.js
@@ -0,0 +1,23 @@
+// Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!".
+// Caso contrário, mostre "Boa semana!".
+
+console.log('>>> Início do programa.');
+
+let dia = prompt('Informe qual o dia semana: ');
+console.log('>>> Informado qual o dia da semana pelo usuário.');
+
+if (dia === 'sabado' || dia === 'Sabado' || dia === 'sábado' || dia === 'Sábado' || dia === 'Domingo' || dia === 'domingo' ) {
+    alert('Bom fim de semana!');
+} else {
+    alert('Boa semana!');
+}
+console.log('>>> Fim do programa.');
+
+// Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+
+// Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!".
+// Caso contrário, mostre "Tente novamente para ganhar.".
+
+// Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+
+// Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
